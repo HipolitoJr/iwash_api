db92755f3c5f4f5c3a5a5366e9b5b0711c893768
HipolitoJr <hipolitocavalcantejr@gmail.com>
2019-01-30 11:39:05 -0300

    Initial commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Hipolito\Documents\ADS_IFPI\2018-2\ENGENHARIA DE SOFTWARE I\PROJETO SCRUM\iwash_api\myenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
