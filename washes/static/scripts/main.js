fd8b39588daa485a9aabc4bb310366ce9ae32129
HipolitoJr <hipolitocavalcantejr@gmail.com>
2019-02-05 17:27:22 -0300

    List solicitacoes

---
+++ washes/static/scripts/main.js
@@ -0,0 +1,40 @@
+var modalConfirm = function(callback, action, param){
+  var action = null
+  $(".action").on("click", function(){
+      action = $(this)
+      $("#msg-confirm").text(action.attr('data-msg'))
+      $("#mi-modal").modal('show');
+  });
+
+  $("#modal-btn-sim").on("click", function(){
+    callback(action.attr('positive'), action.attr('param'));
+    $("#mi-modal").modal('hide');
+  });
+
+  $("#modal-btn-nao").on("click", function(){
+    callback(action.attr('negative'), action.attr('param'));
+    $("#mi-modal").modal('hide');
+  });
+};
+modalConfirm(function(action, param){
+   window[action](param);
+});
+
+
+
+
+
+
+var msgFeedback = function(callback, actionFeedback){
+  var actionFeedback = null
+  $(".actionFeedback").on("click", function(){
+      actionFeedback = $(this)
+      $("#msg-body").text(actionFeedback.attr('data-msg'))
+      $("#feedback-msg").addClass(actionFeedback.attr('data-type-alert'))
+    $("#feedback-msg").css('display', 'block');
+  });
+
+};
+msgFeedback(function(actionFeedback){
+   window[actionFeedback]();
+});
--- washes/views.py
+++ washes/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+
+@login_required
+def index(request):
+    solicitacoes = request.user.lavanderia.solicitacoes.all()
+    return render(request, 'index.html', {'solicitacoes': solicitacoes})
